364ee663ba7bbebd08bbdefc9c28f815b1a03787
annisafitriani254 <annsaftrnii254@gmail.com>
2025-06-04 20:07:34 +0700

    Update tabel.php

--- tabel.php
+++ tabel.php
@@ -94,9 +94,9 @@
   <header>
     <h1>PERPUSTAKAAN ANNISA</h1>
     <nav>
-      <a href="index.html" class="button">Beranda</a>
-      <a href="about.html" class="button">Tentang Saya</a>
-      <a href="formulir.html" class="button">Formulir</a>
+      <a href="index.php" class="button">Beranda</a>
+      <a href="about.php" class="button">Tentang Saya</a>
+      <a href="formulir.php" class="button">Formulir</a>
     </nav>
   </header>
 
@@ -107,22 +107,20 @@
         <caption>Tabel Perkalian</caption>
         <tr>
           <th>x</th>
-          <th>1</th><th>2</th><th>3</th><th>4</th><th>5</th>
-          <th>6</th><th>7</th><th>8</th><th>9</th><th>10</th>
+          <?php for ($i = 1; $i <= 10; $i++) echo "<th>$i</th>"; ?>
         </tr>
-        <tr><th>1</th><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td></tr>
-        <tr><th>2</th><td>2</td><td>4</td><td>6</td><td>8</td><td>10</td><td>12</td><td>14</td><td>16</td><td>18</td><td>20</td></tr>
-        <tr><th>3</th><td>3</td><td>6</td><td>9</td><td>12</td><td>15</td><td>18</td><td>21</td><td>24</td><td>27</td><td>30</td></tr>
-        <tr><th>4</th><td>4</td><td>8</td><td>12</td><td>16</td><td>20</td><td>24</td><td>28</td><td>32</td><td>36</td><td>40</td></tr>
-        <tr><th>5</th><td>5</td><td>10</td><td>15</td><td>20</td><td>25</td><td>30</td><td>35</td><td>40</td><td>45</td><td>50</td></tr>
-        <tr><th>6</th><td>6</td><td>12</td><td>18</td><td>24</td><td>30</td><td>36</td><td>42</td><td>48</td><td>54</td><td>60</td></tr>
-        <tr><th>7</th><td>7</td><td>14</td><td>21</td><td>28</td><td>35</td><td>42</td><td>49</td><td>56</td><td>63</td><td>70</td></tr>
-        <tr><th>8</th><td>8</td><td>16</td><td>24</td><td>32</td><td>40</td><td>48</td><td>56</td><td>64</td><td>72</td><td>80</td></tr>
-        <tr><th>9</th><td>9</td><td>18</td><td>27</td><td>36</td><td>45</td><td>54</td><td>63</td><td>72</td><td>81</td><td>90</td></tr>
-        <tr><th>10</th><td>10</td><td>20</td><td>30</td><td>40</td><td>50</td><td>60</td><td>70</td><td>80</td><td>90</td><td>100</td></tr>
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+          echo "<tr><th>$i</th>";
+          for ($j = 1; $j <= 10; $j++) {
+            echo "<td>" . ($i * $j) . "</td>";
+          }
+          echo "</tr>";
+        }
+        ?>
       </table>
 
-      <a href="index.html" class="back-link">Kembali ke Beranda</a>
+      <a href="index.php" class="back-link">Kembali ke Beranda</a>
     </section>
   </main>
 
@@ -131,4 +129,5 @@
   </footer>
 </body>
 </html>
+
 
